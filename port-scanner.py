11ae3215fa9d6159e07807dbad564690ce3717f4
the8bitbyte <133459899+the8bitbyte@users.noreply.github.com>
2024-02-15 20:45:54 -0500

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -1,6 +1,16 @@
 import sys 
 import socket 
 from datetime import datetime 
+print("""
+
+d8888b.  .d88b.  d8888b. d888888b      .d8888.  .o88b.  .d8b.  d8b   db d8b   db d88888b d8888b. 
+88  `8D .8P  Y8. 88  `8D `~~88~~'      88'  YP d8P  Y8 d8' `8b 888o  88 888o  88 88'     88  `8D 
+88oodD' 88    88 88oobY'    88         `8bo.   8P      88ooo88 88V8o 88 88V8o 88 88ooooo 88oobY' 
+88~~~   88    88 88`8b      88           `Y8b. 8b      88~~~88 88 V8o88 88 V8o88 88~~~~~ 88`8b   
+88      `8b  d8' 88 `88.    88         db   8D Y8b  d8 88   88 88  V888 88  V888 88.     88 `88. 
+88       `Y88P'  88   YD    YP         `8888Y'  `Y88P' YP   YP VP   V8P VP   V8P Y88888P 88   YD 
+                                                                                                 
+""")
 print("Enter your host ip address , Example '192.168.10.10'")
 host = socket.gethostbyname(input("Enter your host ip address: "))
 
